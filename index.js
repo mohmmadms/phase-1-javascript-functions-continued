08732bff01717dcc19131be8ca0ff156a03431f4
mohmmad Smadi <mohmmadmms20@gmail.com>
2023-10-02 01:13:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(value="roller-skate"){
+    return (`This Saturday, I want to ${value}!`)
+}
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+
+ function wrapAdjective(mem="*"){
+    return function(name="special"){
+        return (`You are ${mem}${name}${mem}!`)
+
+    }
+ }
